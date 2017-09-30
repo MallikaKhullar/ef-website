447b2b8efda899b218213a26157dcbe67de4aba7
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-09-30 21:23:07 +0530

    [Database] Local mongo connection set up

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    remoteUrl: 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu', //add our DO mongo URL here
+    localUrl: 'mongodb://127.0.0.1:27017/epic-test'
+};
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express'),
     app = express(),
     port = 8080,
-    router = require('./app/routes');
+    database = require('./config/database'), // load the database config    
+    router = require('./app/routes'),
+    mongoose = require('mongoose'); // mongoose for mongodb
+
 
 //routing the app
 app.use('/', router);
+
+//connect to the local database
+mongoose.connect(database.localUrl);
 
 // static files @ public folder
 app.use(express.static(__dirname + '/public'));
