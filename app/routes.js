1121ed8719e9d95c0006e2cc5af3b8387a6aebd4
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-10-05 15:19:47 +0530

    Setting up Passport auth

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,64 @@
-var express = require('express');
 var path = require('path');
-//create the router object
-var router = express.Router();
 
-//home page with ejs
-router.get('/new-tab', function(req, res) {
-    //TODO: trigger call to database handler to increase #hearts
-    res.render(path.join(__dirname, "../new-tab"));
-});
+module.exports = function(router, passport) {
 
-//about page with ejs
-router.get('/about-us', function(req, res) {
-    res.render(path.join(__dirname, "../about"));
-});
+    router.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
 
-module.exports = router;
+
+    router.get('/login', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    // router.post('/login', do all our passport stuff here);
+
+    router.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // router.post('/signup', do all our passport stuff here);
+
+    router.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user: req.user // get the user out of session and pass to template
+        });
+    });
+
+    //home page with ejs
+    router.get('/new-tab', function(req, res) {
+        //TODO: trigger call to database handler to increase #hearts
+        res.render(path.join(__dirname, "../new-tab"));
+    });
+
+    //about page with ejs
+    router.get('/about-us', function(req, res) {
+        res.render(path.join(__dirname, "../about"));
+    });
+
+    //about page with ejs
+    router.get('/home-page', function(req, res) {
+        res.render(path.join(__dirname, "../login"));
+    });
+
+
+    router.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
--- server.js
+++ server.js
@@ -3,11 +3,25 @@ var express = require('express'),
     port = 8080,
     database = require('./config/database'), // load the database config    
     router = require('./app/routes'),
-    mongoose = require('mongoose'); // mongoose for mongodb
+    mongoose = require('mongoose'), // mongoose for mongodb
+    passport = require('passport'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    flash = require('connect-flash');
 
+app.use(morgan('dev')); // log every request to the console TODO: what?
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-//routing the app
-app.use('/', router);
+//require('./config/passport')(passport);
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret TODO: where to get this from?
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session TODO: find out about this
 
 //view engine is EJS
 app.set('view engine', 'ejs');
@@ -17,6 +31,11 @@ mongoose.connect(database.localUrl);
 
 // static files @ public folder
 app.use(express.static(__dirname + '/public'));
+
+require('./app/routes')(app, passport);
+
+//routing the app
+app.use('/', router);
 
 //start the server
 app.listen(port, function() {
