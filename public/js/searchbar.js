a0ec148153a536b37bb3d2df2f3589426319493b
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-10-10 17:57:58 +0530

    Search bar UI + callback implementation

---
+++ public/js/searchbar.js
@@ -0,0 +1,35 @@
+var suggestCallBack;
+
+$(document).ready(function() {
+
+    $("#autocomplete").autocomplete({
+        source: function(request, response) {
+            $.getJSON("https://suggestqueries.google.com/complete/search?callback=?", {
+                "hl": "en", // Language
+                "jsonp": "suggestCallBack", // jsonp callback function name
+                "q": request.term, // query term
+                "client": "youtube" // force youtube style response, i.e. jsonp
+            });
+            suggestCallBack = function(data) {
+                var len = 0;
+                var suggestions = [];
+                $.each(data[1], function(key, val) {
+                    len++;
+                    suggestions.push({
+                        "value": val[0]
+                    });
+                });
+                suggestions.length = Math.min(len, 5); // prune suggestions list to only 5 items
+                response(suggestions);
+            };
+        },
+        //define select handler
+        select: function(event, ui) {
+            doSearch(ui.item.value);
+        },
+    });
+});
+
+function doSearch(term) {
+    window.location = '//www.google.co.in/search?q=' + term;
+}
