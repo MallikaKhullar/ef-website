bd29f704c399cbfaf2616e06553a21790ea3c7fe
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-10-10 19:17:52 +0530

    Displaying dummy stats for donations / followers

--- app/routes/new-tab/index.js
+++ app/routes/new-tab/index.js
@@ -3,8 +3,20 @@ var router = express.Router();
 var RouteHandler = require('../../handlers/route_handler');
 
 router.get('/', continueIfLoggedIn, function(req, res) {
+
     //TODO: Trigger call to database handler to increase #hearts
-    res.render("new-tab.ejs", { user: req.user });
+    //TODO: fetch stats from donations / followers table
+
+    var data = {
+        user: req.user,
+        stats: {
+            donations: "Rs. 10130.0",
+            followers: "15100"
+        }
+    };
+
+
+    res.render("new-tab.ejs", data);
 });
 
 function continueIfLoggedIn(req, res, next) {
