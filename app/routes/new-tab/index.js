e924dc02de895c0e1be95c6bd955c39a10314d61
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-10-25 16:44:06 +0530

    Adblock detect - backend

--- app/routes/new-tab/index.js
+++ app/routes/new-tab/index.js
@@ -1,22 +1,24 @@
+var adBlocker = require('just-detect-adblock');
 var express = require('express');
 var router = express.Router();
 var RouteHandler = require('../../handlers/route_handler');
 
+
 router.get('/', continueIfLoggedIn, function(req, res) {
 
-    //TODO: Trigger call to database handler to increase #hearts
-    //TODO: fetch stats from donations / followers table
+    if (adBlocker.isDetected()) {
+        res.render("unblock.ejs");
+    } else {
 
-    var data = {
-        user: req.user,
-        stats: {
-            donations: "Rs. 10130.0",
-            followers: "15100"
-        }
-    };
-
-
-    res.render("new-tab.ejs", data);
+        var data = {
+            user: req.user,
+            stats: {
+                donations: "Rs. 10130.0",
+                followers: "15100"
+            }
+        };
+        res.render("new-tab.ejs", data);
+    }
 });
 
 function continueIfLoggedIn(req, res, next) {
