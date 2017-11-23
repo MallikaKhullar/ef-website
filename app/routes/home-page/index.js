6950862f508f991ed11b4d31bc70dd49ebed7402
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-11-23 16:24:09 +0530

    Sending NGO list to home page view

--- app/routes/home-page/index.js
+++ app/routes/home-page/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var RouteHandler = require('../../handlers/route_handler');
+var ngoController = require('../../controllers/ngo');
 
-router.get('/', function(req, res) { res.render("home.ejs"); });
+router.get('/', function(req, res) {
+    ngoController.getAllNgos().pipe(function(data) {
+        res.render("home.ejs", { ngos: data });
+    });
+});
 
 module.exports = router;
