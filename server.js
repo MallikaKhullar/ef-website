3b41d19ed9ad72799648ce596514abfe83e1f0cf
Mallika <foundation.epic@gmail.com>
2017-12-03 14:12:56 +0000

    Increasing TTL

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(session({
     secret: 'deveshIsSexy',
     // create new redis store.
-    store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl :  260}),
+    store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl :  2600}),
     saveUninitialized: false,
     resave: false
 }));
