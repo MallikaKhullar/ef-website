4bdfdff3edd70539599240c0f98b767f7245415b
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-12-03 13:29:34 +0530

    Changing webstore url

--- public/js/chromeExtension.js
+++ public/js/chromeExtension.js
@@ -1,8 +1,10 @@
 function installChromeApp() {
     // chrome.webstore.install();
     chrome.webstore.install(
-        undefined,
-        undefined,
+        "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf",
+        function() {
+            console.log("here");
+        },
         function(err) {
             console.log("no here," + err);
             window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
