9b39ec9cf974c38a5b934b762768f0f4fbabe988
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-12-03 13:16:52 +0530

    Try: Invoking failure callback on chrome install

--- public/js/chromeExtension.js
+++ public/js/chromeExtension.js
@@ -1,11 +1,11 @@
 function installChromeApp() {
-    chrome.webstore.install();
-    // chrome.webstore.install("https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf",
-    //     function() {
-    //         console.log("here");
-    //     },
-    //     function() {
-    //         console.log("no here");
-    //         window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
-    //     });
+    // chrome.webstore.install();
+    chrome.webstore.install("https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf",
+        function() {
+            console.log("here");
+        },
+        function() {
+            console.log("no here");
+            window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
+        });
 }
