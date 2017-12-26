bb66384b921f4793cfbc12f0ef48cd7245882a10
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-12-26 18:43:48 +0530

    Fixing new tab test issue

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,7 @@ var ultron = require('./ultron');
 var blogs = require('./blogs');
 var contactPage = require('./contact');
 var aboutPage = require('./about-us');
+var newTabTest = require('./new-tab-test');
 
 var allRoutes = function(app, passport) {
 
@@ -53,6 +54,7 @@ var allRoutes = function(app, passport) {
     app.use('/new-tab', newTab);
     app.use('/faqs', faqs);
     app.use('/blog', blogs);
+    app.use('/new-tab-test', newTabTest);
 
     app.get('/robots.txt', function(req, res) {
         res.type('text/plain');
@@ -64,6 +66,7 @@ var allRoutes = function(app, passport) {
             "\nDisallow: /blog?type=tech" +
             "\nDisallow: /blog?type=flutur" +
             "\nDisallow: /blog/jlj" +
+            "\nDisallow: /blog/mall" +
             "\nDisallow: /blog/ngos_dog_adoption");
     });
 
---
+++ app/routes/new-tab-test/index.js
@@ -0,0 +1,118 @@
+var adBlocker = require('just-detect-adblock');
+var express = require('express');
+var router = express.Router();
+var RouteHandler = require('../../handlers/route_handler');
+var User = require('../../repo/mongo/user');
+var Utils = require('../../utils');
+var userController = require('../../controllers/user');
+var donationController = require('../../controllers/donations');
+var causeController = require('../../controllers/cause');
+var ngoController = require('../../controllers/ngo');
+var deferred = require('./../../utils/deferred');
+var fn = require('./../../utils/functions');
+var moment = require('moment');
+
+router.get('/mission-selected', continueIfLoggedIn, function(req, res) {
+    if (req.query && req.query.cause_id) {
+        var start = moment().format('x');
+        var end = Utils.getEndTime(start);
+        userController.setCause(req.user.user_id, req.query.cause_id, start, end).pipe(function(data) {
+            res.redirect('/new-tab');
+        });
+    } else res.redirect('/new-tab');
+});
+
+
+router.get('/choose-mission', continueIfLoggedIn, function(req, res) {
+    causeController.getAllCauses().pipe(function(data) {
+        res.render("select-cause.ejs", { data });
+    });
+});
+
+
+function redirectForNewUser(user, res) {
+    if (user.state === "uninitiated") {
+
+        causeController.getAllCauses().pipe(function(data) {
+            res.render("select-cause.ejs", { data });
+        });
+        return true;
+    }
+    return false;
+}
+
+router.get('/', continueIfLoggedIn, function(req, res) {
+
+    if (redirectForNewUser(req.user, res)) return;
+
+    var def = {
+        userCount: userController.getAllUserCount(),
+        donationCount: donationController.getAllDonationCount(),
+        currentCause: causeController.getCauseFromId(req.user.hearts.current_cause_id),
+        previousCause: causeController.getCauseFromId(req.user.previous_donation.previous_cause_id)
+    };
+
+    deferred.combine(def).pipe(function(data) {
+        constructPayload({
+            user: req.user,
+            cause: data.currentCause,
+            numDonations: data.donationCount,
+            numUsers: data.userCount,
+            previousCause: data.previousCause
+        }).pipe(function(result) {
+            res.render("new-tab.ejs", result);
+        });
+    });
+
+    userController.incrementHearsById(req.user.id);
+});
+
+
+function constructPayload(data) {
+    var hoursToGo = Utils.timePeriodInHours(data.user.hearts.target_end_time, moment().format('x'));
+    var remainingTime = hoursToGo >= 24 ? (hoursToGo / 24 > 1 ? Math.floor(hoursToGo / 24) + " days " : Math.floor(hoursToGo / 24) + " day ") :
+        (hoursToGo > 1 ? Math.floor(hoursToGo) + " hours " : "1 hour ");
+    var progress = Utils.calculateProgress(data.user.hearts.current_week_hearts, data.cause.total_hearts);
+    data.user.progress = progress;
+
+    var newdata = {
+        user: data.user,
+        cause: data.cause,
+        previousCause: data.previousCause || {},
+        stats: {
+            donations: "Rs. " + Utils.getCommaSeparatedMoney(data.numDonations),
+            followers: Utils.getCommaSeparatedNumber(data.numUsers),
+            remainingTime: remainingTime
+        }
+    };
+
+    if (data.user.state == 'cause_selection_pending') {
+        return ngoController.getNgosFromCauseId(data.previousCause.cause_id).pipe(function(res) {
+            newdata.previousCause.ngoList = res;
+            return deferred.success(newdata);
+        });
+    }
+
+    newdata.timeElapsed = moment().format('x') > data.user.hearts.target_end_time;
+
+    if (newdata.timeElapsed &&
+        data.user.state !== "cause_selection_pending" &&
+        data.user.state !== 'donate_pending_dismissed') {
+
+        newdata.user.state = "donate_pending";
+
+        return ngoController.getNgosFromCauseId(data.cause.cause_id).pipe(function(res) {
+            newdata.cause.ngoList = res;
+            return deferred.success(newdata);
+        });
+
+    } else return deferred.success(newdata);
+}
+
+function continueIfLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/login');
+}
+
+
+module.exports = router;
