1b220c19264007ebefda83cd52bd21dd28fb18ef
Mallika Priya Khullar <mallika@icecreamlabs.com>
2018-04-12 21:45:00 +0530

    Changing metric growth

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -28,14 +28,14 @@ exports.getAllDonationCount = function() {
             var night = moment(extra + '23:59');
 
             var REV_PER_TAB = 0.07,
-                ADBLOCK_DISABLED = 0.7,
+                ADBLOCK_DISABLED = 0.35,
                 DAU_PER_30D_INSTALLS = 0.7,
                 TABS_PER_USER_MIN = 0.011;
 
             TIME_PASSED = (moment().format('x') - user.timestamp) / (1000 * 60);
 
             var incremental_rev = REV_PER_TAB * TABS_PER_USER_MIN * USER_COUNT * DAU_PER_30D_INSTALLS * ADBLOCK_DISABLED;
-            var total_rev = (incremental_rev * TIME_PASSED / 2);
+            var total_rev = 11000 + (incremental_rev * TIME_PASSED / 2);
 
             return deferred.success(total_rev.toFixed(2));
         });
