1264d4c75cf99b77be2fa4dfeafc337a1b210c52
Mallika Priya Khullar <mallika@icecreamlabs.com>
2018-04-17 17:16:25 +0530

    Fixing controller exports

--- app/controllers/new-tab-endpoint-controller.js
+++ app/controllers/new-tab-endpoint-controller.js
@@ -9,6 +9,95 @@ var moment = require('moment');
 var Utils = require('./../utils');
 const prettyMs = require('pretty-ms');
 
+/**
+ * -------------------------------------- CONTROLLER RENDERERS ------------------------------------------- *
+ */
+
+exports.render_shiftUsersToV1 = function(user, res) {
+    var def = {
+        projects: projectController.getProjectOverviews({ truncShortDesc: true, truncShort: 70, truncHomeDesc: true, truncHome: 200 }),
+        donations: donationController.getAllStats(user.user_id)
+    };
+
+
+    deferred.combine(def).pipe(function(data) {
+
+        var newdata = {};
+        newdata = Utils.appendProjects(newdata, data.projects);
+
+        var causesIds = data.donations.map(a => a._id);
+        causeController.getAllCausesByIds(causesIds).pipe(function(allCauses) {
+            for (i in allCauses) {
+                allCauses[i].user_progress = findUserProgress(data.donations, allCauses[i].cause_id);
+            }
+            newdata.causes = allCauses;
+            res.render("move-on.ejs", newdata);
+        });
+    });
+}
+
+exports.render_weekOngoing_v1 = function(req, res, setAsDonatePending) {
+    userController.incrementTabsById(req.user.id);
+    if (setAsDonatePending) userController.setDonatePending(req.user.user_id);
+
+    var def = {
+        userCount: userController.getAllUserCount(),
+        donationCount: donationController.getAllDonationCount(),
+        currentProject: projectController.getProjectDetails({ projectId: req.user.project.project_id })
+    };
+
+    deferred.combine(def).pipe(function(data) {
+        v1_constructPayload({
+            user: req.user,
+            numDonations: data.donationCount,
+            numUsers: data.userCount,
+            project: data.currentProject,
+            req: req,
+        }).pipe(function(result) {
+            result.showMissionSelectedPopup = false;
+            res.render("project-new-tab.ejs", result);
+        });
+    });
+}
+
+exports.render_autoAssignNewProject = function(req, res, showMissionSelectedPopup) {
+    console.log("auoassign");
+    userController.incrementTabsById(req.user.id);
+
+    var def = {
+        userCount: userController.getAllUserCount(),
+        donationCount: donationController.getAllDonationCount(),
+        currentProject: projectController.getFeaturedProject() //for new users, assign them featured project
+    };
+
+    deferred.combine(def).pipe(function(data) {
+
+        var start = moment().format('x');
+        var end = Utils.getTwoWeekTime(start);
+        userController.setProject(req.user.user_id, data.currentProject.projectId, start, end).pipe(function(proj) {
+
+            // req.user.state = "v1_week_ongoing";
+
+            v1_constructPayload({
+                user: req.user,
+                numDonations: data.donationCount,
+                numUsers: data.userCount,
+                project: data.currentProject,
+                req: req,
+            }).pipe(function(result) {
+                result.showMissionSelectedPopup = showMissionSelectedPopup;
+                console.log("austosdsds");
+                res.render("project-new-tab.ejs", result);
+            });
+        });
+    });
+}
+
+
+/**
+ * -------------------------------------- UTILS ------------------------------------------- *
+ */
+
 function getFirstAlphabet(str) {
     for (var i = 0; i < str.length; i++) {
         if (str[i] >= 'A' && str[i] <= 'z') return str[i];
@@ -50,7 +139,6 @@ function appendProjectProgress(data) {
     if (data.project.weeklyTargetTabs) {
         var progress = Utils.calculateProgress(data.user.project.tabs, data.project.weeklyTargetTabs);
         data.user.progress = progress;
-        console.log("progress", progress, "% ", data.user.project.tabs, data.project.weeklyTargetTabs);
     }
     return data;
 }
@@ -59,6 +147,12 @@ function appendBackgroundImage(newdata) {
     var daysPassed = Utils.timePeriodInDays(moment().format('x'), newdata.user.timestamp);
     newdata.dailyImage = constants.images[Math.round(daysPassed) % constants.images.length];
     return newdata;
+}
+
+function findUserProgress(causes, id) {
+    for (i in causes) {
+        if (causes[i]._id == id) return causes[i].hearts;
+    }
 }
 
 function v1_constructPayload(data) {
@@ -112,112 +206,4 @@ function v1_constructPayload(data) {
     }
 
     return deferred.success(newdata);
-}
-
-
-exports.render_shiftUsersToV1 = function(user, res) {
-    var def = {
-        projects: projectController.getProjectOverviews({ truncShortDesc: true, truncShort: 70, truncHomeDesc: true, truncHome: 200 }),
-        donations: donationController.getAllStats(user.user_id)
-    };
-
-    deferred.combine(def).pipe(function(data) {
-        var newdata = {};
-        newdata = Utils.appendProjects(newdata, data.projects);
-
-        var causesIds = data.donations.map(a => a._id);
-        causeController.getAllCausesByIds(causesIds).pipe(function(allCauses) {
-            for (i in allCauses) {
-                allCauses[i].user_progress = findUserProgress(data.donations, allCauses[i].cause_id);
-            }
-            newdata.causes = allCauses;
-            console.log("FINAL OBJECT", newdata);
-            res.render("move-on.ejs", newdata);
-        });
-    });
-}
-
-exports.render_weekOngoing_v1 = function(req, res, setAsDonatePending) {
-    userController.incrementTabsById(req.user.id);
-    if (setAsDonatePending) userController.setDonatePending(req.user.user_id);
-
-    var def = {
-        userCount: userController.getAllUserCount(),
-        donationCount: donationController.getAllDonationCount(),
-        currentProject: projectController.getProjectDetails({ projectId: req.user.project.project_id })
-    };
-
-    deferred.combine(def).pipe(function(data) {
-        console.log("2");
-        v1_constructPayload({
-            user: req.user,
-            numDonations: data.donationCount,
-            numUsers: data.userCount,
-            project: data.currentProject,
-            req: req,
-        }).pipe(function(result) {
-            result.showMissionSelectedPopup = false;
-            console.log("FINAL OBJECT", result);
-            res.render("project-new-tab.ejs", result);
-        });
-    });
-}
-
-exports.render_weekOngoing_v0 = function(req, res) {
-
-    var def = {
-        userCount: userController.getAllUserCount(),
-        donationCount: donationController.getAllDonationCount(),
-        currentCause: causeController.getCauseFromId(req.user.hearts.current_cause_id),
-        previousCause: causeController.getCauseFromId(req.user.previous_donation.previous_cause_id)
-    };
-
-    deferred.combine(def).pipe(function(data) {
-
-        v0_constructPayload({
-            user: req.user,
-            cause: data.currentCause,
-            numDonations: data.donationCount,
-            numUsers: data.userCount,
-            previousCause: data.previousCause,
-            req: req,
-        }).pipe(function(result) {
-            console.log("FINAL OBJECT", result);
-            res.render("new-tab.ejs", result);
-        });
-    });
-}
-
-exports.render_autoAssignNewProject = function(req, res, showMissionSelectedPopup) {
-    console.log("auoassign");
-    userController.incrementTabsById(req.user.id);
-
-    var def = {
-        userCount: userController.getAllUserCount(),
-        donationCount: donationController.getAllDonationCount(),
-        currentProject: projectController.getFeaturedProject() //for new users, assign them featured project
-    };
-
-    deferred.combine(def).pipe(function(data) {
-
-        var start = moment().format('x');
-        var end = Utils.getTwoWeekTime(start);
-        userController.setProject(req.user.user_id, data.currentProject.projectId, start, end).pipe(function(proj) {
-
-            // req.user.state = "v1_week_ongoing";
-
-            v1_constructPayload({
-                user: req.user,
-                numDonations: data.donationCount,
-                numUsers: data.userCount,
-                project: data.currentProject,
-                req: req,
-            }).pipe(function(result) {
-                result.showMissionSelectedPopup = showMissionSelectedPopup;
-                console.log("FINAL OBJECT", result);
-                console.log("austosdsds");
-                res.render("project-new-tab.ejs", result);
-            });
-        });
-    });
 }
--- app/controllers/v0-new-tab-endpoint-controller.js
+++ app/controllers/v0-new-tab-endpoint-controller.js
@@ -7,19 +7,11 @@ var fn = require('./../utils/functions');
 var constants = require('./../utils/constants');
 var moment = require('moment');
 var Utils = require('./../utils');
+const prettyMs = require('pretty-ms');
 
 
 
-
-//v0 route
-exports.findUserProgress = function(causes, id) {
-    for (i in causes) {
-        if (causes[i]._id == id) return causes[i].hearts;
-    }
-}
-
-
-exports.constructPayload = function(data) {
+function constructPayload(data) {
     var redirected = data.req != undefined && data.req.query != undefined && data.req.query.redirect != undefined ? JSON.parse(data.req.query.redirect.toLowerCase()) : false;
 
     var hoursToGo = Utils.timePeriodInHours(data.user.hearts.target_end_time, moment().format('x'));
@@ -46,7 +38,6 @@ exports.constructPayload = function(data) {
         redirected: redirected
     };
 
-
     var total_hearts_text = data.user.hearts.total_hearts == 1 ? " heart" : " hearts";
 
     var daysPassed = Utils.timePeriodInDays(moment().format('x'), data.user.timestamp);
@@ -55,7 +46,6 @@ exports.constructPayload = function(data) {
     newdata.user.first_name = Utils.firstName(newdata.user.name);
     newdata.user.picture = (newdata.user.picture == null || newdata.user.picture == undefined) ? "/image/user.png" : newdata.user.picture;
     newdata.user.hearts.total_hearts_text = data.user.hearts.total_hearts + total_hearts_text;
-
 
     if (data.user.state == 'cause_selection_pending') {
         return ngoController.getNgosFromCauseId(data.previousCause.cause_id).pipe(function(res) {
@@ -71,5 +61,31 @@ exports.constructPayload = function(data) {
             return deferred.success(newdata);
         });
 
-    } else return deferred.success(newdata);
+
+    } else {
+        return deferred.success(newdata);
+    }
+}
+
+exports.render_weekOngoing_v0 = function(req, res) {
+    var def = {
+        userCount: userController.getAllUserCount(),
+        donationCount: donationController.getAllDonationCount(),
+        currentCause: causeController.getCauseFromId(req.user.hearts.current_cause_id),
+        previousCause: causeController.getCauseFromId(req.user.previous_donation.previous_cause_id)
+    };
+
+    deferred.combine(def).pipe(function(data) {
+
+        constructPayload({
+            user: req.user,
+            cause: data.currentCause,
+            numDonations: data.donationCount,
+            numUsers: data.userCount,
+            previousCause: data.previousCause,
+            req: req,
+        }).pipe(function(result) {
+            res.render("new-tab.ejs", result);
+        });
+    });
 }
