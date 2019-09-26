1fa0b8516bb692c2c6fa583da2023a17ac6f19e5
Mallika <mallika@tinystep.in>
2019-09-26 15:18:12 -0700

    Changing URL for Chrome extension

--- public/js/chromeExtension.js
+++ public/js/chromeExtension.js
@@ -1,16 +1,17 @@
 function installChromeApp() {
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    var isDesktop = window.matchMedia('(min-width: 700px)').matches;
+    window.open('https://chrome.google.com/webstore/detail/flutur-new-tab/onebagjpomhjadhoiianmnelkbcnllnf?hl=en','_newtab');
+    // var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    // var isDesktop = window.matchMedia('(min-width: 700px)').matches;
 
-    if (!isChrome || !isDesktop) {
-        window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
-        return;
-    }
+    // if (!isChrome || !isDesktop) {
+    //     window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
+    //     return;
+    // }
 
-    chrome.webstore.install(
-        "https://chrome.google.com/webstore/detail/onebagjpomhjadhoiianmnelkbcnllnf",
-        function() {},
-        function(err) {
-            window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
-        });
+    // chrome.webstore.install(
+    //     "https://chrome.google.com/webstore/detail/onebagjpomhjadhoiianmnelkbcnllnf",
+    //     function() {},
+    //     function(err) {
+    //         window.location.href = "https://chrome.google.com/webstore/detail/flutur-small-flutters-cau/onebagjpomhjadhoiianmnelkbcnllnf";
+    //     });
 }
